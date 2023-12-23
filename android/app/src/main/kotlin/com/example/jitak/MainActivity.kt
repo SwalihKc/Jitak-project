03426dcf3f25f55c337bff20d29c0fd9866204c8
SwalihKc <swalihkc123@gmail.com>
2023-12-23 15:18:19 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/jitak/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.jitak
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:get/get_connect/http/src/utils/utils.dart';
+import 'package:jitak/view/mobile/edit_store_profile_mobile.dart';
+import 'package:jitak/view/mobile/home_page_mobile.dart';
+import 'package:jitak/view/mobile/stamb_card_details_mobile.dart';
+import 'package:jitak/view/tab/homePageTablet.dart';
+import 'package:jitak/view/tab/stamp_card_details_tablet.dart';
+import 'package:responsive_builder/responsive_builder.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home:   ScreenTypeLayout.builder(
+          mobile: (p0) => const HomePage(),
+          tablet: (p0) => const HomePageTablet()
+        )
+        );
+  }
+}
+
